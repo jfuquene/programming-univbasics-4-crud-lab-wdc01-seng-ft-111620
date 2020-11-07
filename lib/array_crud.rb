8f9b53b838a38653694011acb2fb1cc53e6d870a
John Fuquene <jfuquene@email.wm.edu>
2020-11-07 00:31:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-   first_salad["lettuce", "spinach", "cheese"]
-  add_element_to_start_of_array.push("corn")
+   salad = ["lettuce", "spinach", "cheese"]
+  salad.push("corn")
   
 end
 
